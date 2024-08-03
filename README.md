a4b9402a76b23b706d92be98f8cb46d3988858e3
Acero Liang <acero.liangli@stonybrook.edu>
2024-08-02 22:01:30 -0400

    put server on aws

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # sign-languague-api
+
+Demo a simple endpoint. 
+
+### Endpoint 
+
+POST /translate: 
+```
+- content-type: josn
+- Body: { "text": "Your text here" }
+```
+
+**web endpoint**: ``
+
+**locally**: `http:localhost:3500/translate`
+
+### JS example with axios
+```javascript
+axios.post('http://localhost:3500/translate', {
+  text: 'Hello world'
+})
+```
+
+
+
---
+++ dist/server.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const port = 3500;
+/** listen: launch port **/
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+/** '/translate': api endpoint for receiving text input in English. Returns status code 200 if successful translation **/
+app.post('/translate', (req, res) => {
+    try {
+        const { text } = req.body;
+        if (!text) {
+            throw new Error('Missing text field');
+        }
+        // we are going to 'fake' translation. Actual translation would go here
+        setTimeout(() => {
+            res.status(200).json({ message: "Successful translation:" });
+            console.log(text);
+        }, 2000);
+    }
+    catch (error) {
+        res.status(400).json({ error: "Invalid input" });
+    }
+});
